45403dda716b4aabb71aa73e25f6fd7556194fa0
ali taslimpoor <shayannniiii@gmail.com>
2023-04-22 13:37:05 +0300

    solved

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code here
+function moveDodgerLeft() {
+    let dodger = document.getElementById("dodger");
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    
+    if (left > 0) {
+      dodger.style.left = `${left - 4}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    let dodger = document.getElementById("dodger");
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    
+    if (left < 360) {
+      dodger.style.left = `${left + 4}px`;
+    }
+  }
+  
